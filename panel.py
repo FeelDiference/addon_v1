3c579c67ee5b77be5dcdcab0a14573912d8d01a3
Maksim <derbison@yandex.ru>
2025-04-22 21:51:32 +0000

    test

--- panel.py
+++ panel.py
@@ -1,7 +1,7 @@
 import bpy
 
 class PIVOTMAPPER_PT_panel(bpy.types.Panel):
-    bl_label = "Pivot Mapper"
+    bl_label = "Pivot HUAAAPEper"
     bl_idname = "PIVOTMAPPER_PT_panel"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
